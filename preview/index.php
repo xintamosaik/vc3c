96a5a953d9d10bf0f3418f207db8856f687c332d
Ulf <xintamosaik@gmail.com>
2025-04-13 23:03:19 +0000

    move to data based job section

--- preview/index.php
+++ preview/index.php
@@ -73,7 +73,7 @@ $location = $personal_data['location'] ?? '';
         </span>
         <span class="dash"></span>
         <span>
-           <?php echo $location; ?>
+            <?php echo $location; ?>
         </span>
     </section>
 
@@ -91,102 +91,49 @@ $location = $personal_data['location'] ?? '';
     <section id="experience">
         <h2>Experience</h2>
 
-        <div class="job entry">
-            <h3>
-                <span>Web Developer</span>
-                <span class="dash"></span>
-                <span>iq digital media marketing gmbh</span>
-            </h3>
-            <h4>
-                <span>Jan 2024 - Present</span>
-                <span class="dash"></span>
-                <span>Düsseldorf, Germany</span>
-            </h4>
-
-            <ul>
-                <li>
-                    Introduced and promoted TypeScript to enhance code quality and decrease error occurrences.
-                </li>
-                <li>
-                    Created a <strong>template builder</strong>, saving <strong>30%</strong> of development time and optimizing workflow.
-                </li>
-                <li>
-                    Implemented the <strong>'skyMover'</strong> system to enhance ad placement reliability and remove <strong>4,000 lines</strong> of code.
-                </li>
-                <li>
-                    Developed an ad controller to facilitate CI/CD processes and decrease dependence on third-party systems.
-                </li>
-                <li>
-                    Developed a Chrome extension that helps clients and product managers to quickly see health metrics of their websites.
-                </li>
-                <li>
-                    Technologies: <strong>React, TypeScript, Tailwind, Vite, AWS, Github Actions</strong>
-                </li>
-            </ul>
-
-        </div>
-
-        <div class="job entry">
-            <h3>
-                <span>Full Stack Developer</span>
-                <span class="dash"></span>
-                <span>Qanuk GmbH</span>
-            </h3>
-            <h4>
-                <span>April 2021 - December 2023</span>
-                <span class="dash"></span>
-                <span>Osnabrück, Niedersachsen, Germany</span>
-            </h4>
-
-            <ul>
-
-                <li>
-                    Developed a scalable and dynamic search UI that connected 3rd party APIs and scraped articles using Elastic.
-                </li>
-                <li>
-                    Created a geolocation-based search UI for medical practitioners by developing custom WordPress plugins.
-                </li>
-                <li>
-                    Oversaw the creation of a WordPress plugin to streamline invoice management and event-related tasks.
-                </li>
-                <li>
-                    Integrated <strong>AJAX, jQuery, and REST APIs</strong> to enhance interactivity and performance.
-                </li>
-                <li>
-                    Provided mentorship and fostered collaborative development practices, increasing productivity.
-                </li>
-                <li>
-                    Technologies: <strong>PHP, WordPress, JavaScript, jQuery, AJAX, React, Docker</strong>
-                </li>
-            </ul>
-
-        </div>
         <?php
         $job_files = glob($_SERVER['DOCUMENT_ROOT'] . '/data/jobs/*.json');
         foreach ($job_files as $file) {
             $job_json = file_get_contents($file);
             $job_data = json_decode($job_json, true);
-            print_r($job_data);
+
             $job_title = $job_data['position'] ?? '';
             $company = $job_data['company'] ?? '';
             $start_date = $job_data['start'] ?? '';
             $end_date = $job_data['end'] ?? '';
             $location = $job_data['location'] ?? '';
-            $description = $job_data['description'] ?? '';
+            $description_data = $job_data['description'] ?? '';
+            $description_broken = nl2br(htmlspecialchars($description_data));
+            $description_array = explode("\n", $description_broken);
 
-            echo "<div class='job entry'>";
-            echo "<h3>";
-            echo "<span>$job_title</span>";
-            echo "<span class='dash'></span>";
-            echo "<span>$company</span>";
-            echo "</h3>";
-            echo "<h4>";
-            echo "<span>$start_date - $end_date</span>";
-            echo "<span class='dash'></span>";
-            echo "<span>$location</span>";
-            echo "</h4>";
-            echo "<p>$description</p>";
-            echo "</div>";
+            $description = '';
+            foreach ($description_array as $line) {
+                if (trim($line) === '') {
+                    continue; // Skip empty lines
+                }
+                $description .= "<li>$line</li>";
+            }
+
+            echo <<<HTML
+                <div class="job entry">
+                    <h3>
+                        <span>$job_title</span>
+                        <span class="dash"></span>
+                        <span>$company</span>
+                    </h3>
+                    <h4>
+                        <span>$start_date - $end_date</span>
+                        <span class="dash"></span>
+                        <span>$location</span>
+                    </h4>
+
+                    <ul>
+                    
+                            $description
+                    
+                    </ul>
+                </div>
+            HTML;
         }
         ?>
     </section>
